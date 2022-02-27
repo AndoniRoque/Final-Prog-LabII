d48fb2f87dc48a3bc1387cd1feb6c639ad5a85df
Juan Fullana <juanfullana6@gmail.com>
2022-02-27 10:14:54 -0300

    Added login to front page

---
+++ main.js
@@ -0,0 +1,33 @@
+// Loop through Array of Objects
+var objPeople = [
+	{
+		username: "Juan",
+		password: "Bajo"
+	},
+	{
+		username: "Carlos",
+		password: "Berger"
+	},
+	{
+		username: "Andoni",
+		password: "Roque"
+	},
+    {
+		username: "Juancito",
+		password: "Fullana"
+	}
+
+]
+
+function getInfo() {
+	var username = document.getElementById('username').value
+	var password = document.getElementById('password').value
+
+	for(var i = 0; i < objPeople.length; i++) {
+		if(username == objPeople[i].username && password == objPeople[i].password) {
+			window.location.href = "http://stackoverflow.com";
+			return
+		}
+	}
+	console.log("incorrect username or password")
+}
