4b6b24b0f02638ede302cc9c5706f56da0444282
Andoni Roque <roque.andoni@gmail.com>
2022-03-02 17:15:57 -0300

    index working, needs polishing

--- js/controller.js
+++ js/controller.js
@@ -2,7 +2,7 @@ fetch('http://127.0.0.1:5000/movies')
     .then( res => res.json())
     .then( movies => {
         clear_node('side_bar');
-        
+
         pos = movies.length - 1;
         i = 0;
 
@@ -18,18 +18,32 @@ fetch('http://127.0.0.1:5000/movies')
             }
         }
 
-        let num = Math.floor(Math.random() * movies.length - 2);
+        let num = Math.floor(Math.random() * movies.length - 1);
 
         if (num < 0){
             num = num * -1;
         }
-        
-        console.log("the random number is: " + num)
 
-        for (let i=0; i < 5; i++){
-            console.log(num);
-            main_bar(movies[num].Title, movies[num].Year, movies[num].Director, movies[num].Genre, movies[num].Synopsis, movies[num].Poster);
-            num += 1;
+        for (let i=0; i < 6; i++){
+            let cont = document.getElementById('titles');
+            cont.innerHTML +=
+                `<div class="movie_info">
+                    <div class="poster_main">
+                        <img src=${movies[num].Poster}>
+                    </div>
+                    <div class="tech_file">
+                        <p> <strong> Title: </strong>${movies[num].Title}</p> 
+                        <p> <b> Director: </b> ${movies[num].Director} </p>
+                        <p> <b> Year: </b> ${movies[num].Year}</p>
+                        <p> <b> Synopsis: </b> ${movies[num].Synopsis}</p>
+                    </div>
+                </div>` 
+            num++
+            console.log(num)
+            if (num >= movies.length){
+                num = 0;
+            }
+            console.log(num)
         }
     })
 
@@ -60,52 +74,5 @@ function latest_added(title, year, poster){
     cont.appendChild(pTitle);
 
     let c = document.getElementById('side_bar');
-    c.appendChild(cont);
-}
-
-function main_bar(title, year, director, genre, synopsis, poster){
-    console.log("PELICULAS: " + title + year + director + genre + synopsis + poster);
-    let cont = document.createElement('div');
-    cont.classList.add('movie_info');
-
-    let cont_img = document.createElement('div');
-    cont_img.classList.add('poster_main');
-
-    let img = document.createElement('img');
-    img.src = poster;
-    cont_img.appendChild(img);
-
-    let hTitle = document.createElement('h2');
-    hTitle.classList.add('Title');
-
-    let txtTitle = document.createTextNode('Title: ' + title);
-
-    let info = document.createElement('div');
-    info.classList.add('tech_file');
-
-    let pYear = document.createElement('p');
-    let txtYear = document.createTextNode("Year: " + year);
-    let pDirector = document.createElement('p');
-    let txtDirector = document.createTextNode("Director: " + director);
-    let pGenre = document.createElement('p');
-    let txtGenre = document.createTextNode("Genre: " + genre);
-    let pSynopsis = document.createElement('p');
-    let txtSnopsis = document.createTextNode("Synopsis: " + synopsis);
-
-    hTitle.appendChild(txtTitle);
-    pDirector.appendChild(txtDirector);
-    pYear.appendChild(txtYear);
-    pGenre.appendChild(txtGenre);
-    pSynopsis.appendChild(txtSnopsis);
-
-    info.appendChild(hTitle);
-    info.appendChild(pDirector);
-    info.appendChild(pYear);
-    info.appendChild(pGenre);
-    info.appendChild(pSynopsis);
-
-    cont.appendChild(cont_img);
-    cont.appendChild(info);
-    let c = document.getElementById('titles');
     c.appendChild(cont);
 }
