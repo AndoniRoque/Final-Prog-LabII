879498b69f84f68445aa0e5b794c35fce16c3f38
Andoni Roque <roque.andoni@gmail.com>
2022-03-05 17:30:33 -0300

    edit page working with PUT and POST methods
    It still lacks integration with the rest of the site but it works on a vacum, probably needs another set of eyes to go over the code and verify it makes sense

--- js/edit.js
+++ js/edit.js
@@ -1,4 +1,5 @@
 var btn_up = document.getElementById('upload');
+var btn_ed = document.getElementById('edit');
 
 btn_up.addEventListener('click', event =>{
     event.preventDefault();
@@ -15,7 +16,7 @@ btn_up.addEventListener('click', event =>{
     let newPoster = inputs[3].value;
     
     let newMovie = {
-        Title: newTitle,
+        Title: newTitle, 
         Director: newDirector,
         Year: newYear,
         Genre: newGenre,
@@ -23,7 +24,7 @@ btn_up.addEventListener('click', event =>{
         Poster: newPoster
     }
     
-    console.log(newMovie);
+    // console.log(newMovie);
     
     let requestOptions = {
         method: 'POST',
@@ -39,4 +40,62 @@ btn_up.addEventListener('click', event =>{
             // btn_up.click(event);
         })
 
+})
+
+let newId = 0;
+
+btn_ed.addEventListener('click', event =>{
+    event.preventDefault();
+    let formData = document.getElementById('formData');
+    let inputs = formData.getElementsByTagName('input');
+    let select_d = document.getElementById('director');
+    let select_g = document.getElementById('genre');
+
+    let newTitle = inputs[0].value;
+    let newDirector = select_d.options[select_d.selectedIndex].value;
+    let newYear = inputs[1].value;
+    let newGenre = select_g.options[select_g.selectedIndex].value;
+    let newSynopsis = inputs[2].value;
+    let newPoster = inputs[3].value;
+
+    let edit_movie = {
+        Title: newTitle,
+        Director: newDirector,
+        Year: newYear,
+        Genre: newGenre,
+        Synopsis: newSynopsis,
+        Poster: newPoster,
+        id: newId
+    }
+
+    let requestOptions = {
+        method: 'PUT',
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify(edit_movie)
+    }
+
+    fetch('http://127.0.0.1:5000/movies')
+        .then( res => res.json())
+        .then( movies => {
+            console.log(movies);
+            data = movies
+
+        // localStorage.setItem('movie_list', JSON.stringify(movies));
+
+    let movie_edit = movies.filter( movie => movie.id == newId);
+    let idEdit = movie_edit[0].id;
+
+    console.log("url del enpoint = ", `http://127.0.0.1:5000/movies/edit/${idEdit}`);
+    
+    fetch(`http://127.0.0.1:5000/movies/edit/${idEdit}`, requestOptions)
+        .then( resp => resp.json())
+        .catch( error => alert(error))
+        .then( data =>{
+            console.log("Data is: ", data);
+        })
+    
+    })
+   
+
+    
 })
