82156a57b7ea4bcca1fc9db0208a391ee2489bf2
Juan Fullana <juanfullana6@gmail.com>
2022-03-07 12:47:48 -0300

    Json Update-Unnecesary files-Movie.js
    Updated Json, removed unnecesary files, edited movie.js

--- js/movie.js
+++ js/movie.js
@@ -13,10 +13,11 @@ fetch('http://127.0.0.1:5000/movies/'+id)
                 <iframe class="trailer" width="560" height="400" src="${data.Trailer}"frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
             <div class="info_box">
-            <p>Hola</p>
-            <p>Hola</p>
-            <p>Hola</p>
-            <p>Hola</p>
-            <p>Hola</p>
+            <p>${data.Title}</p>
+            <p>${data.Year}</p>
+            <p>${data.Director}</p>
+            <p>${data.Genre}</p>
+            <p>${data.Synopsis}</p>
+            <p>${data.Cast}</p>
         `;
 })
